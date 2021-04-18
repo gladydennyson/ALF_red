d7c6c80b961eb5940e9dfb4dfdbee80f5f036889
Glady Dennyson Thomas <gladydennyson184@gmail.com>
2021-04-17 19:57:44 -0500

    Logging framework initial commit

---
+++ src/test/java/com/example/alfred/AlfredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alfred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

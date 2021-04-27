f00ccd5174d57404d014c1994a9cd6e6a70cf102
Akshay <narulaakshay01@gmail.com>
2021-04-27 11:48:24 -0500

    improved custom exceprion and found an alternative to health checks

--- src/main/java/com/example/alfred/exception/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/example/alfred/exception/ExceptionHandlerControllerAdvice.java
@@ -5,24 +5,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 
 @EnableWebMvc
 @ControllerAdvice
 public class ExceptionHandlerControllerAdvice {
 
-	@ExceptionHandler(AspectException.class)
-	@ResponseBody
-    public final ResponseEntity<Object> handleAllAspectExceptions(AspectException ex, WebRequest request) {
-        System.out.println(1);
-        return new ResponseEntity<Object>(ex.getMessage(), ex.getExceptionStatus());
-    }
-	
 	@ExceptionHandler(Exception.class)
 	@ResponseBody
-    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
-        System.out.println(2);
-        return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		ResponseStatus annotation = AnnotatedElementUtils.findMergedAnnotation(ex.getClass(), ResponseStatus.class);
+		if (annotation != null) {
+			status = annotation.value()!=null?annotation.value():annotation.code();
+		}
+		System.out.println(2);
+		return new ResponseEntity<Object>(ex.getMessage(), status);
+	}
 }

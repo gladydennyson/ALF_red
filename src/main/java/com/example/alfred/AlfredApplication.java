67317e38bd08a8009cceb23b29a2f0ef5cbcadcd
glady <gladydennyson184@gmail.com>
2021-04-27 22:40:06 -0500

    Logger tests

--- src/main/java/com/example/alfred/AlfredApplication.java
+++ src/main/java/com/example/alfred/AlfredApplication.java
@@ -1,12 +1,18 @@
 package com.example.alfred;
 
+import com.example.alfred.logger.EventLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlfredApplication {
-	public static void main(String[] args) {
 
+	//test
+	private static EventLogger logger = new EventLogger();
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		logger.debug("test event log");
 		SpringApplication.run(AlfredApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/alfred/logger/EventLogger.java
@@ -0,0 +1,36 @@
+package com.example.alfred.logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Throwables;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventLogger{
+
+    private Logger logger;
+
+    public EventLogger(){
+        this.logger = LogManager.getLogger("eventLog");
+    }
+
+    public void debug(String message) {
+
+        logger.log(Level.DEBUG,  message);
+    }
+
+    public void info(String message) {
+
+        logger.log(Level.INFO, message);
+    }
+
+    public void warn(String message) {
+
+        logger.log(Level.WARN, message);
+    }
+
+
+}
